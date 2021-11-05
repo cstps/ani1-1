5718bc1052d9835db90c32a4ebe99ba7ac50e56f
seotosna <79702470+cstps@users.noreply.github.com>
2021-11-05 03:00:48 +0000

    로봇 이동경로 상에 선 그리기, 플레이, 정지, 다시실행버튼 완성

--- ani/static/js/main.js
+++ ani/static/js/main.js
@@ -1,5 +1,3 @@
-
-
 let canvas = document.createElement("canvas");
 canvas.setAttribute("width", "512");
 canvas.setAttribute("height", "512");
@@ -8,69 +6,180 @@ let codingArea = document.getElementById('draw')
 codingArea.appendChild(canvas);
 
 let ctx = canvas.getContext("2d");
+
+
+var textarea = document.getElementById('editor');
+var editor = CodeMirror.fromTextArea(textarea, {
+    lineNumbers: true,
+    val: textarea.value,
+    mode : "javascript",
+});
+
+
+// 바닥 한 타일에 대한 정보
 class Tile{
+  // 타일의 x좌표, 타일의 y좌표, 그림이미지 개수, 그림이미지 소스
   constructor(x,y,cnt,source){
+    // 벽면 테두리 여부
     this.border = {
       east:0,
       south:0,
       west:0,
       north:0,
     };
+    // 바닥색상, 그림이미지 개수, 그림이미지 소스
     this.data = {
       color:"blank",
       count:cnt,
-      imgsrc:source,
+      imageSrc:source,
     };
-    this.postition ={
+    // 타일 위치정보
+    this.position ={
       tileX:x,
       tileY:y,
     };
   }
+  tileDraw(){
+    let tileImage = new Image();
+    tileImage.src = "/static/images/"+this.data.imageSrc;
+    ctx.drawImage(tileImage,this.position.tileX,this.position.tileY,64,64);
+  }
 }
 
-let fps = 1,
- start = 0,
- frameDuration = 1000 / fps;
-let catImage = new Image();
-catImage.src = "/static/images/cat.png"
 
-let ob1 = new Tile(32,32,1,"/static/images/cat.png");
+// 로봇 클래스 생성
 
-let bg = new Image();
-bg.src = ob1.data.imgsrc;
-let imageX = 0;
+class Robot{
+  constructor(x,y,direction,imageSrc="ant.png"){
+    // 로봇의 시작좌표
+    this.startx = x;
+    this.starty = y;
+    this.prex = x;
+    this.prey = y;
+    this.x = x;
+    this.y = x;
+    this.direction = direction;
+    this.imageSrc = imageSrc;
+    this.width = 64;
+    this.height = 64;
+  }
+  init(){
+    ctx.clearRect(this.prex,this.prey,this.width, this.height);
+    ctx.moveTo(this.startx + this.width/2, this.starty+this.height/2);
+    this.x = this.startx;
+    this.y = this.starty;
+    this.prex = this.x;
+    this.prey = this.y;
+  }
+  draw(){    
+    ctx.clearRect(this.prex,this.prey,this.width, this.height);
+    let roboImage = new Image();
+    roboImage.src = "/static/images/"+this.imageSrc;
+    ctx.drawImage(roboImage,this.x,this.y,this.width,this.height);
+    ctx.lineTo(this.x+this.width/2, this.y + this.height/2);
+    ctx.stroke();
+    this.prex = this.x;
+    this.prey = this.y;
+  }
+  move(){
+    this.x+=this.width;
+  }
+  tr(){
+    
+  }
+
+}
+
+// 실행속도 제어
+let fps = 1, start = 0, frameDuration = 1000 / fps;
+
+// 초기 설정
+let robot = new Robot(0,0,0);
+let tile = new Array();
 let requestAniNum=0;
 
+// 첫 월드 화면이 로딩 되었을 때
+let worldInit = function(){ 
+  // 화면 초기화
+  ctx.clearRect(0,0,canvas.width, canvas.height);
+
+  //로봇 초기 위치 설정
+  robot.init();
+  robot.draw();
+
+  // 타일 초기 설정
+  tile.push(new Tile(64,64,1,"bee.png"));
+  tile.push(new Tile(64*2,64,1,"ant.png"));
+  tile.push(new Tile(64*3,64,1,"bird.png"));
+
+  // 타일 보이기
+  for(let i=0; i<3; i+=1){
+    tile[i].tileDraw();
+    console.log(tile[i].data.imageSrc);
+  }
+}
+
+worldInit();
 
 
+// 코드를 다시 실행했을 경우 기존 로봇과 물건만 원래 상태로 변경하기.
+let worldReInit = function(){ 
+  // 화면 초기화
+  ctx.clearRect(0,0,canvas.width, canvas.height);
+  ctx.beginPath();
+  //로봇 초기 위치 설정
+  robot.init();
+  robot.draw();
+
+  // 타일 보이기
+  for(let i=0; i<3; i+=1){
+    tile[i].tileDraw();
+    console.log(tile[i].data.imageSrc);
+  }
+}
 
 
-gameLoop();
 //Start the game loop
 function gameLoop(timestamp) {
  requestAniNum = requestAnimationFrame(gameLoop);
  
  // 애니메이션 시간 설정
  if (timestamp >= start) {
- //update the game logic
- ctx.clearRect(0,0,canvas.width, canvas.height);
- 
- ctx.drawImage(catImage,imageX,0,32,32);
- ctx.drawImage(bg,ob1.postition.tileX,ob1.postition.tileY,32,32);
- imageX+=32;
- //ob1.draw(ctx);
- //Reset the frame start time
- start = timestamp + frameDuration;
+
+  // 로봇 그리기
+  robot.draw();
+  robot.move();
+
+     // 타일 그리기
+  for(let i=0;i<3;i+=1)
+    tile[i].tileDraw();
+
+  
+  //Reset the frame start time
+  start = timestamp + frameDuration;
  }
 }
 
-canvas.addEventListener('mousedown',restart,false);
+let playbutton = document.getElementById('playbutton')
+let stopbutton = document.getElementById('stop')
+let repeatbutton = document.getElementById('repeat')
 
-function restart(){
-  cancelAnimationFrame(requestAniNum);
-  imageX = 0;
+playbutton.addEventListener('mousedown', robotPlay,false);
+stopbutton.addEventListener('mousedown', robotStop,false);
+repeatbutton.addEventListener('mousedown', robotRepeat,false);
+
+function robotPlay(e){  
   gameLoop();
-};
+}
 
+function robotStop(e){  
+  cancelAnimationFrame(requestAniNum);
+}
+
+function robotRepeat(e){  
+  cancelAnimationFrame(requestAniNum);
+  worldReInit();
+  gameLoop();
+}
 
 
