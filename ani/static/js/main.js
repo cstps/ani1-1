1c43dcf322eff3205f5f2597d114b04e4796e491
seotosna <79702470+cstps@users.noreply.github.com>
2021-11-10 07:23:55 +0000

    ace_edit add lib/ace파일추가 . script load

--- ani/static/js/main.js
+++ ani/static/js/main.js
@@ -1,20 +1,7 @@
-let canvas = document.createElement("canvas");
-canvas.setAttribute("width", "512");
-canvas.setAttribute("height", "512");
-canvas.style.border = "1px dashed black";
-let codingArea = document.getElementById('draw')
-codingArea.appendChild(canvas);
-
-let ctx = canvas.getContext("2d");
 
 
-var textarea = document.getElementById('editor');
-var editor = CodeMirror.fromTextArea(textarea, {
-    lineNumbers: true,
-    val: textarea.value,
-    mode : "javascript",
-});
-
+let canvas 
+let ctx
 
 // 바닥 한 타일에 대한 정보
 class Tile{
@@ -39,7 +26,7 @@ class Tile{
       tileY:y,
     };
   }
-  tileDraw(){
+  tileDraw(ctx){
     let tileImage = new Image();
     tileImage.src = "/static/images/"+this.data.imageSrc;
     ctx.drawImage(tileImage,this.position.tileX,this.position.tileY,64,64);
@@ -50,7 +37,7 @@ class Tile{
 // 로봇 클래스 생성
 
 class Robot{
-  constructor(x,y,direction,imageSrc="ant.png"){
+  constructor(x,y,direction,imageSrc="ant"){
     // 로봇의 시작좌표
     this.startx = x;
     this.starty = y;
@@ -63,7 +50,7 @@ class Robot{
     this.width = 64;
     this.height = 64;
   }
-  init(){
+  init(ctx){
     ctx.clearRect(this.prex,this.prey,this.width, this.height);
     ctx.moveTo(this.startx + this.width/2, this.starty+this.height/2);
     this.x = this.startx;
@@ -71,10 +58,10 @@ class Robot{
     this.prex = this.x;
     this.prey = this.y;
   }
-  draw(){    
+  draw(ctx){    
     ctx.clearRect(this.prex,this.prey,this.width, this.height);
     let roboImage = new Image();
-    roboImage.src = "/static/images/"+this.imageSrc;
+    roboImage.src = "/static/images/"+ this.imageSrc + this.direction +".png";
     ctx.drawImage(roboImage,this.x,this.y,this.width,this.height);
     ctx.lineTo(this.x+this.width/2, this.y + this.height/2);
     ctx.stroke();
@@ -82,45 +69,53 @@ class Robot{
     this.prey = this.y;
   }
   move(){
-    this.x+=this.width;
+    if(this.direction == 0) // 동쪽
+      this.x+=this.width;
+    else if(this.direction == 1) // 북쪽
+      this.y-=this.width;
+    else if(this.direction == 2) // 서쪽
+      this.x-=this.width;
+    else if(this.direction == 3) // 남쪽
+      this.y+=this.width;
   }
-  tr(){
-    
+  tr(){ // 왼쪽 돌기
+    this.direction = (this.direction+1)% 4;
   }
-
 }
-
 // 실행속도 제어
 let fps = 1, start = 0, frameDuration = 1000 / fps;
 
 // 초기 설정
 let robot = new Robot(0,0,0);
-let tile = new Array();
+let tile = [];
 let requestAniNum=0;
 
-// 첫 월드 화면이 로딩 되었을 때
-let worldInit = function(){ 
-  // 화면 초기화
-  ctx.clearRect(0,0,canvas.width, canvas.height);
+canvas = document.getElementById('canvas')  ;
+canvas.height = 512;
+canvas.width =  512;
+canvas.style.border = "1px dashed black";
+ctx = canvas.getContext("2d");
 
-  //로봇 초기 위치 설정
-  robot.init();
-  robot.draw();
+//로봇 초기 위치 설정
+robot.init(ctx);
+robot.draw(ctx);
 
-  // 타일 초기 설정
-  tile.push(new Tile(64,64,1,"bee.png"));
-  tile.push(new Tile(64*2,64,1,"ant.png"));
-  tile.push(new Tile(64*3,64,1,"bird.png"));
+// 타일 초기 설정
+tile.push(new Tile(64,64,1,"bee.png"));
+tile.push(new Tile(64*2,64,1,"ant.png"));
+tile.push(new Tile(64*3,64,1,"bird.png"));
 
-  // 타일 보이기
-  for(let i=0; i<3; i+=1){
-    tile[i].tileDraw();
-    console.log(tile[i].data.imageSrc);
-  }
+// 타일 보이기
+for(let i=0; i<3; i+=1){
+  tile[i].tileDraw(ctx);
+  console.log(tile[i].data.imageSrc);
 }
 
-worldInit();
 
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.session.setMode("ace/mode/javascript");
+editor.setFontSize(20);
 
 // 코드를 다시 실행했을 경우 기존 로봇과 물건만 원래 상태로 변경하기.
 let worldReInit = function(){ 
@@ -128,14 +123,15 @@ let worldReInit = function(){
   ctx.clearRect(0,0,canvas.width, canvas.height);
   ctx.beginPath();
   //로봇 초기 위치 설정
-  robot.init();
-  robot.draw();
+  robot.init(ctx);
+  robot.draw(ctx);
 
   // 타일 보이기
   for(let i=0; i<3; i+=1){
-    tile[i].tileDraw();
+    tile[i].tileDraw(ctx);
     console.log(tile[i].data.imageSrc);
   }
+  
 }
 
 
@@ -147,12 +143,12 @@ function gameLoop(timestamp) {
  if (timestamp >= start) {
 
   // 로봇 그리기
-  robot.draw();
+  robot.draw(ctx);
   robot.move();
 
      // 타일 그리기
   for(let i=0;i<3;i+=1)
-    tile[i].tileDraw();
+    tile[i].tileDraw(ctx);
 
   
   //Reset the frame start time
@@ -178,7 +174,7 @@ function robotStop(e){
 
 function robotRepeat(e){  
   cancelAnimationFrame(requestAniNum);
-  worldReInit();
+  worldReInit(ctx);
   gameLoop();
 }
 
